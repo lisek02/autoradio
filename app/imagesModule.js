59f8af02fd905315751c3a7c41fb5a74c03af042
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-03-31 23:56:51 +0200

    add parameter to showImage directive

--- app/imagesModule.js
+++ app/imagesModule.js
@@ -1,14 +1,19 @@
 (function(){
 angular.module('imagesModule', [])
+	.controller('ImagesController', ['$scope', function($scope) {
+	}])
 
 	.directive('showImage', function(){
 		return {
 			restrict: 'E',
+			scope: {
+				source: '@',
+			},
 			templateUrl: 'app/views/show-image.html',
 			link: function($scope, iElm, iAttrs, controller) {
 				
 			}
 		};
 	});
-	
+
 })();
