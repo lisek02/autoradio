f33b835ffe294db1f93c9e176e969ffc936ee7fb
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-05-08 04:00:08 +0200

    improve warning label animation

--- app/contactModule.js
+++ app/contactModule.js
@@ -2,16 +2,12 @@
 angular.module('contactModule', [])
 
 	.controller('contactController', ['$scope', function($scope){
-		this.message = {};
+		this.message;
 		this.submitted = false;
-		this.formRowNumbers = "col-md-12";
-		this.messageRowNumbers = "col-md-0";
 
 		this.sendMessage = function(contact) {
 			this.submitted = true;
 			this.message = angular.copy(contact);
-			this.formRowNumbers = "col-md-10";
-			this.messageRowNumbers = "col-md-2";
 		};
 
 	}])
